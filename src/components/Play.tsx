1b79de3701d1f22730cc3d43ab37b86cd857696a
Jon <gravityrideseverything1@gmail.com>
2025-05-05 21:59:15 -0400

    use piano voicing from tonejs

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as Tone from 'tone';
 import Box from '@mui/material/Box'
 import styled from 'styled-components'
 import Button from '@mui/material/Button'
+import { notes } from '../util';
 
 interface Props {
   notes: string[]
@@ -24,13 +25,52 @@ const Container = styled(Box)`
 
 const Play: React.FC<Props> = ({ notes, isSlashChord }) => {
   const [playing, setPlaying] = useState<boolean>(false);
+  const [sampler, setSampler] = useState<Tone.Sampler | null>(null);
 
-  const synth = new Tone.PolySynth(Tone.AMSynth, {
-    harmonicity: 1,
-    oscillator: {
-      type: 'triangle10'
-    }
-  }).toDestination();
+  useEffect(() => {
+    const piano = new Tone.Sampler({
+      urls: {
+        A0: "A0.mp3",
+        C1: "C1.mp3",
+        "D#1": "Ds1.mp3",
+        "F#1": "Fs1.mp3",
+        A1: "A1.mp3",
+        C2: "C2.mp3",
+        "D#2": "Ds2.mp3",
+        "F#2": "Fs2.mp3",
+        A2: "A2.mp3",
+        C3: "C3.mp3",
+        "D#3": "Ds3.mp3",
+        "F#3": "Fs3.mp3",
+        A3: "A3.mp3",
+        C4: "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        A4: "A4.mp3",
+        C5: "C5.mp3",
+        "D#5": "Ds5.mp3",
+        "F#5": "Fs5.mp3",
+        A5: "A5.mp3",
+        C6: "C6.mp3",
+        "D#6": "Ds6.mp3",
+        "F#6": "Fs6.mp3",
+        A6: "A6.mp3",
+        C7: "C7.mp3",
+        "D#7": "Ds7.mp3",
+        "F#7": "Fs7.mp3",
+        A7: "A7.mp3",
+        C8: "C8.mp3"
+      },
+      baseUrl: "https://tonejs.github.io/audio/salamander/",
+      onload: () => {
+        setSampler(piano);
+      }
+    }).toDestination();
+
+    return () => {
+      piano.dispose();
+    };
+  }, []);
 
   const increaseNotesOctives = (notes: string[]): string[] => {
     return notes.map((note: string, index: number) => {
@@ -51,15 +91,17 @@ const Play: React.FC<Props> = ({ notes, isSlashChord }) => {
   }
 
   const playNote = () => {
-    const frequencies = increaseNotesOctives(notes).map(note => Tone.Frequency(note).toFrequency());
-    synth.triggerAttackRelease(frequencies, '2');
+    if (!sampler) return;
+
+    const frequencies = increaseNotesOctives(notes);
+    sampler.triggerAttackRelease(frequencies, '2');
     setPlaying(true);
     setTimeout(() => setPlaying(false), 2000)
   }
 
   return (
     <Container>
-      <Button onClick={playNote} disabled={playing}>Play</Button>
+      <Button onClick={playNote} disabled={playing || !sampler}>Play</Button>
     </Container>
   )
 }
